ed298ddec9d2330ba9bb761b331c4fbe217a7b78
Nandhuarumalla <arumallanandini32@gmail.com>
2024-10-31 15:47:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//Function to show a specific section and hide others
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+        if (section.id === sectionId) {
+            section.classList.add('active');
+        }
+    });
+}
